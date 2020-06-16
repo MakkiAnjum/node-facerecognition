ae91fbe9477f54fa16eebeeb4b37e204fefd8cde
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-06-16 15:14:34 +0500

    ssl

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
